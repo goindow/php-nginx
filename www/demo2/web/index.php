37a57837176bc05eee4e35ae104f4657b830ac92
hyb <76788424@qq.com>
2020-06-12 14:16:37 +0800

    modify add mongodb ext(pkg-config libcurl4-openssl-dev libssl-dev)

--- www/demo2/web/index.php
+++ www/demo2/web/index.php
@@ -1,2 +1,12 @@
 <?php
-    echo "demo2";
+
+    #$manager = new MongoDB\Driver\Manager("mongodb://hyb:123456@47.111.0.166:27017/test");
+    $manager = new MongoDB\Driver\Manager("mongodb://hyb:123456@192.168.0.102:27017/test");
+
+    $query = new MongoDB\Driver\Query([]);
+    
+    $cursor = $manager->executeQuery('test.user', $query);
+
+    foreach ($cursor as $document) {
+        print_r($document);
+    }
