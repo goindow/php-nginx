45877d23230d49915afc510e084e8942f0b84104
hyb <76788424@qq.com>
2021-05-25 10:23:45 +0800

    modify demo2

--- www/demo2/web/index.php
+++ www/demo2/web/index.php
@@ -1,12 +1,2 @@
 <?php
-
-    #$manager = new MongoDB\Driver\Manager("mongodb://hyb:123456@47.111.0.166:27017/test");
-    $manager = new MongoDB\Driver\Manager("mongodb://hyb:123456@10.0.235.231:27017/test");
-
-    $query = new MongoDB\Driver\Query([]);
-    
-    $cursor = $manager->executeQuery('test.user', $query);
-
-    foreach ($cursor as $document) {
-        print_r($document);
-    }
+    echo "demo2";
